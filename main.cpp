3f1bb9da1f4af790d7a4e0393bc818c3eabd8e40
DitlevHB <naranix@hotmail.com>
2018-02-02 09:50:45 +0100

    init

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <conio.h>
+
+//Standard namespace declaration
+using namespace std;
+
+//Main Function
+int main()
+{
+    int x = 2;
+
+    cout << "Hello";
+
+    getch();
+
+    return 0;
+}
